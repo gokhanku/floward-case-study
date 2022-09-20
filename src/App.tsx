b403454ef92b5b03ca26f6b1c5c11b1aa9f3a88e
gokhan.kurt <gokhan.kurt@caretta.net>
2022-09-20 15:16:25 +0300

    changed project configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import CountryListing from "./pages/country-listing";
+import CountryDetails from "./pages/country-detail";
+import Login from "./pages/login";
+import NotFound from "./pages/not-found";
+import ProtectedRoute from "./hoc/protected-route";
+import { Constants } from "./core/variables/constants";
+import PublicRoute from "./hoc/public-route";
 
 function App() {
+  const token = localStorage.getItem(Constants.TOKEN);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <CountryListing />
+          </ProtectedRoute>
+        }
+      >
+        {/* <Route path="details/:alphaCode" element={<CountryDetails/>} /> */}
+      </Route>
+      <Route
+        path="details/:alphaCode"
+        element={
+          <ProtectedRoute>
+            <CountryDetails />
+          </ProtectedRoute>
+        }
+      ></Route>
+      <Route
+        path="/login"
+        element={
+          <PublicRoute>
+            <Login />
+          </PublicRoute>
+        }
+      />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
